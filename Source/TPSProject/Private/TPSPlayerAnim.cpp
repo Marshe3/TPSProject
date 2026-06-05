cb8dce87e93854290e00b95aaef3ab713854ef4b
Marshe3 <rkddus97@gmail.com>
2026-06-05 15:37:03 +0900

    feat : 애니메이션 인스턴스 구성 Speed 변수, NativeUpdateAnimation을 통한 속도 반영

---
+++ Source/TPSProject/Private/TPSPlayerAnim.cpp
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TPSPlayerAnim.h"
+
+#include "TPSPlayer.h"
+
+// 매 프레임 자동 호출 - Tick 직접 구성만해도 UE 자체가 호출
+void UTPSPlayerAnim::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+	
+	// 이 Anumation Instance를 소유한 폰 (TPSPlayer) 가져오기
+	APawn* pawn = TryGetPawnOwner();
+	if (pawn == nullptr) return;
+	
+	// ATPSPlayer 폰의 속도 측정
+	ATPSPlayer* player = Cast<ATPSPlayer>(pawn);
+	if (player)
+	{
+		speed = player->GetVelocity().Size();
+	}
+	
+	// [DEBUG]
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1,0.f, FColor::Green, FString::Printf(TEXT("Anim : %f"), speed));
+	}
+}
